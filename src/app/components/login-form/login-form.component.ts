47df3a78a4d47b68db6a8cd013ff4ebfde2bd745
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-15 00:16:17 +0000

    Create form login

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss'
+})
+export class LoginFormComponent {
+
+  loginForm = new FormGroup({
+    inputEmail: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(2)
+    ]),
+    inputPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/(?=.*[A-Z])/),
+      Validators.pattern(/(?=.*[a-z])/),
+      Validators.pattern(/(?=.*[0-9])/),
+      Validators.pattern(/(?=.*[!@#$%^&*])/),
+    ])
+  })
+
+  login(){
+    const email = this.loginForm.value.inputEmail ? this.loginForm.value.inputEmail : "";
+    const password = this.loginForm.value.inputPassword ? this.loginForm.value.inputPassword : "";
+
+    console.log(email + "-" + password);
+  }
+}
