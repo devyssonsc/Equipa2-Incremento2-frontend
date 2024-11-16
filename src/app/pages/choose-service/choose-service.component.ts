7a8de8b17178d4db95778fb7398dfdb2a1d22483
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-16 15:24:14 +0000

    Update choose service page

--- src/app/pages/choose-service/choose-service.component.ts
+++ src/app/pages/choose-service/choose-service.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { ServiceOptionComponent } from '../../components/service-option/service-option.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-choose-service',
@@ -12,26 +13,50 @@ import { ServiceOptionComponent } from '../../components/service-option/service-
 export class ChooseServiceComponent {
     datas = [
       {
+        id: 1,
         title: "Pintura",
         description: "Pintura em casas ou apartamentos com até 100m2",
-        pro: "Divaldo Dias",
+        pro: {
+          name: "Divaldo Dias",
+          specialty: "Pintura",
+          experience: 3,
+          requests: []
+        },
         price: "25,00"
       },
       {
+        id: 2,
         title: "Marcenaria",
         description: "Manutenção em móveis feitos de madeira",
-        pro: "Tiago",
+        pro: {
+          name: "Tiago Silva",
+          specialty: "Marcenaria",
+          experience: 1,
+          requests: []
+        },
         price: "13,00"
       },
       {
+        id: 3,
         title: "Eletricidade",
         description: "Troca de lâmpadas e/ou tomadas",
-        pro: "Tomás David",
+        pro: {
+          name: "Tomás David",
+          specialty: "Eletricidade",
+          experience: 5,
+          requests: []
+        },
         price: "9,49"
       }
     ]
 
+    constructor(private router: Router){}
+
     onSearch(){
       alert("Pesquisando...");
+    }
+
+    showDetails(id: number){
+      this.router.navigate(['/service-option', id]);
     }
 }
