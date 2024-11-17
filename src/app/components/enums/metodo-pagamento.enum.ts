e307c3c1314e7e5b555a9a502a27a6b7f29471f1
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-17 20:09:34 +0000

    Update metodo-pagamento.enum.ts

--- src/app/components/enums/metodo-pagamento.enum.ts
+++ src/app/components/enums/metodo-pagamento.enum.ts
@@ -1,6 +1,6 @@
 export enum MetodoPagamento {
-    MBWAY = 'MBWay',
-    CARTAO_DE_CREDITO = 'Cartão de crédito',
-    TRANSFERENCIA_BANCARIA = 'Transferência Bancária',
-    PAYPAL = 'PayPal',
+    MBWAY = 'MBWAY',
+    CARTAO_DE_CREDITO = 'CARTAO_DE_CREDITO',
+    TRANSFERENCIA_BANCARIA = 'TRANSFERENCIA_BANCARIA',
+    PAYPAL = 'PAYPAL',
 }
