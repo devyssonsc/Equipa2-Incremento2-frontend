aa558cfc2904a481d54614b5c2e4a9c82735489d
Devysson Cardoso <devyssonsc@gmail.com>
2024-11-19 21:21:21 +0000

    Update reg-form.component.ts

--- src/app/components/reg-form/reg-form.component.ts
+++ src/app/components/reg-form/reg-form.component.ts
@@ -80,7 +80,7 @@ export class RegFormComponent implements OnInit {
       morada: this.registerForm.value.inputAddress ? this.registerForm.value.inputAddress : "",
       userType: this.registerForm.value.checkUserType ? this.registerForm.value.checkUserType.toUpperCase() : "",
       formaDePagamento: this.registerForm.value.inputPaymentMethod ? this.registerForm.value.inputPaymentMethod : "",
-      especialidade: this.registerForm.value.inputSpecialty ? this.registerForm.value.inputSpecialty : "",
+      especialidade: this.registerForm.value.inputSpecialty ? this.registerForm.value.inputSpecialty : null,
       experiencia: this.registerForm.value.inputExperience ? this.registerForm.value.inputExperience : "",
     }
     console.log(formData);
